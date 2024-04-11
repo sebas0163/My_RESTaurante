e7f0add841593477e7853d46e223302c45231a9b
Crawford Barquero <fabian152195@gmail.com>
2024-04-11 03:46:47 -0600

    [ADD] backup

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ app.use(express.json()); // Middleware to parse JSON requests
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
-app.use('/api', require('./BackendCore/RoutingComponent'));
+//app.use('/api', require('./BackendCore/RoutingComponent'));
+app.use('/api', require('./FoodRecomendation/FoodCore'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
