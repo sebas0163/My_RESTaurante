65d4ab70dd6f913c580db7753b0d43d8425d1545
Jespinoza1703 <38987724+Jespinoza1703@users.noreply.github.com>
2024-04-11 13:11:11 -0600

    Frontend (#5)
    * [ADD] Created components and configured routing
    
    * [ADD] Implemented menu option selection
    
    * [ADD] Implemented datepicker for time recommendation
    
    * [UPDATE] Get value from feedback textbox
    
    * [ADD] Created and implemented get functions for menu with mock API
    
    * [UPDATE] Implemented feedback get and tested with local api
    
    * [ADD/DELETE] Created service to pass url and deleted extra component
    
    * Auto stash before merge of "Frontend" and "origin/Frontend"
    
    * Auto stash before merge of "Frontend" and "origin/Frontend"
    
    * [UPDATE] Corrected typo and changed feedback url to final version
    
    * [UPDATE] Integrated local API for time recommendation with Frontend

--- WebApp/WebApp/src/app/components/time-recommendation/time-recommendation.component.ts
+++ WebApp/WebApp/src/app/components/time-recommendation/time-recommendation.component.ts
@@ -1,20 +1,72 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { MyServiceService } from '../../my-service.service';
+import moment from 'moment';
 
 
 @Component({
   selector: 'app-time-recommendation',
   templateUrl: './time-recommendation.component.html',
-  styleUrls: ['../../app.component.scss']
+  styleUrls: ['../../app.component.scss'],
+  
 })
 export class TimeRecommendationComponent {
 
+  showRecommendationContent: boolean = false;
 
   selectedDate: Date | null = null; // Initialize the property directly
+  times: any = [];
+  baseUrl: any;
 
-  getRecommendation() {
-    console.log(this.selectedDate);
-    if(this.selectedDate) console.log(this.selectedDate.toLocaleDateString()); // Transform Date to string using default locale
-
+  
+  constructor(private myService: MyServiceService, private http: HttpClient) {
+    this.baseUrl = this.myService.getTimeUrl();
   }
+
+  getDateRecommendation(){
+    if(this.selectedDate){
+      this.showRecommendationContent = true;
+      console.log(this.selectedDate.toLocaleDateString())
+    }
+    try {
+        this.http.get<any>('apiUrl').subscribe(
+          (response) => {
+            console.log(response);
+            this.times = response.times;
+          },
+          (error) => {
+            console.error('Error:', error);
+          }
+        )
+    } catch (error) {
+      console.error('Fetch error:', error);
+      // Propagate the error by rethrowing it
+      throw error;
+    }
+  }
+
+
+  postDateRecommendation() {
+
+    if (this.selectedDate) {
+      this.showRecommendationContent = true;
+
+      const body = {
+        "day": this.selectedDate.toLocaleDateString()
+      }
+
+      // Make the POST request
+      this.http.post<any>(this.baseUrl, body).subscribe(
+        (response) => {
+          this.times = response;
+          this.times = this.times.map((item: moment.MomentInput) => moment(item).format('MMMM D, YYYY, h:mm A'));
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
+    }
+  }
+
+
 }
