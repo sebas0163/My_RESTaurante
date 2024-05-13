918053ac58f5435ec810ca32efc7c47466c3e13b
Kevin Zeledón <kzeledon@protonmail.com>
2024-05-13 10:36:02 -0600

    Lint issues

--- BackendCore/ReservationController.js
+++ BackendCore/ReservationController.js
@@ -1,77 +1,77 @@
 const { PubSubReceiverSender } = require("../common/PubSub");
 
 class ReservationIface {
-	constructor(
-		sender_topic_name = "reservation-downstream",
-		receiver_topic_name = "reservation-upstream",
-		subscription_name = "ReservationController-sub",
-	) {
-		this.pubSubHandler = new PubSubReceiverSender(
-			sender_topic_name,
-			receiver_topic_name,
-			subscription_name,
-		);
-	}
+  constructor(
+    sender_topic_name = "reservation-downstream",
+    receiver_topic_name = "reservation-upstream",
+    subscription_name = "ReservationController-sub",
+  ) {
+    this.pubSubHandler = new PubSubReceiverSender(
+      sender_topic_name,
+      receiver_topic_name,
+      subscription_name,
+    );
+  }
 
-	async getReservationResponse(message) {
-		this.pubSubHandler.send_message(JSON.stringify(message));
-		const upstream_res = await this.pubSubHandler().pull_single_message();
-		console.log("Got upstream res");
-		return JSON.parse(upstream_res);
-	}
+  async getReservationResponse(message) {
+    this.pubSubHandler.send_message(JSON.stringify(message));
+    const upstream_res = await this.pubSubHandler().pull_single_message();
+    console.log("Got upstream res");
+    return JSON.parse(upstream_res);
+  }
 }
 
 class ReservationController {
-	constructor() {
-		this.reservation_interface = new ReservationIface();
-		this.createReservation = this.createReservation.bind(this);
-		this.deleteReservation = this.deleteReservation.bind(this);
-		this.getAllReservations = this.getAllReservations.bind(this);
-		this.getReservationById = this.getReservationById.bind(this);
-	}
-	createReservation(req, res) {
-		const people = req.body.people;
-		const time = req.body.timeid;
-		const user = req.body.userid;
-		const reserv_obj = {
-			message_code: 2,
-			people: people,
-			timeid: time,
-			userid: user,
-		};
-		this.reservation_interface
-			.getReservationResponse(reserv_obj)
-			.then((reserv_res) => {
-				res.status(reserv_res.status).json(reserv_res.data);
-			});
-	}
-	getAllReservations(req, res) {
-		const reserv_obj = { message_code: 0 };
-		this.reservation_interface
-			.getReservationResponse(reserv_obj)
-			.then((reserv_res) => {
-				res.status(reserv_res.status).json(reserv_res.data);
-			});
-	}
-	deleteReservation(req, res) {
-		const res_id = req.body.id;
-		console.log(req.body.id);
-		const reserv_obj = { message_code: 1, id: res_id };
-		this.reservation_interface
-			.getReservationResponse(reserv_obj)
-			.then((reserv_res) => {
-				res.status(reserv_res.status).json(reserv_res.data);
-			});
-	}
-	getReservationById(req, res) {
-		const res_id = req.body.id;
-		const reserv_obj = { message_code: 3, id: res_id };
-		this.reservation_interface
-			.getReservationResponse(reserv_obj)
-			.then((reserv_res) => {
-				res.status(reserv_res.status).json(reserv_res.data);
-			});
-	}
+  constructor() {
+    this.reservation_interface = new ReservationIface();
+    this.createReservation = this.createReservation.bind(this);
+    this.deleteReservation = this.deleteReservation.bind(this);
+    this.getAllReservations = this.getAllReservations.bind(this);
+    this.getReservationById = this.getReservationById.bind(this);
+  }
+  createReservation(req, res) {
+    const people = req.body.people;
+    const time = req.body.timeid;
+    const user = req.body.userid;
+    const reserv_obj = {
+      message_code: 2,
+      people: people,
+      timeid: time,
+      userid: user,
+    };
+    this.reservation_interface
+      .getReservationResponse(reserv_obj)
+      .then((reserv_res) => {
+        res.status(reserv_res.status).json(reserv_res.data);
+      });
+  }
+  getAllReservations(req, res) {
+    const reserv_obj = { message_code: 0 };
+    this.reservation_interface
+      .getReservationResponse(reserv_obj)
+      .then((reserv_res) => {
+        res.status(reserv_res.status).json(reserv_res.data);
+      });
+  }
+  deleteReservation(req, res) {
+    const res_id = req.body.id;
+    console.log(req.body.id);
+    const reserv_obj = { message_code: 1, id: res_id };
+    this.reservation_interface
+      .getReservationResponse(reserv_obj)
+      .then((reserv_res) => {
+        res.status(reserv_res.status).json(reserv_res.data);
+      });
+  }
+  getReservationById(req, res) {
+    const res_id = req.body.id;
+    const reserv_obj = { message_code: 3, id: res_id };
+    this.reservation_interface
+      .getReservationResponse(reserv_obj)
+      .then((reserv_res) => {
+        res.status(reserv_res.status).json(reserv_res.data);
+      });
+  }
 }
 
 module.exports = { ReservationController };
--- Reservation/ReservationCore.js
+++ Reservation/ReservationCore.js
@@ -2,130 +2,130 @@ const { DatabaseController } = require("../common/DatabaseController");
 const { PubSubSender } = require("../common/PubSub");
 
 class ReservationCore {
-	constructor() {
-		this.databaseController = new DatabaseController();
-		this.pubSubHandler = new PubSubSender("reservation-upstream");
-	}
+  constructor() {
+    this.databaseController = new DatabaseController();
+    this.pubSubHandler = new PubSubSender("reservation-upstream");
+  }
 
-	async process_message(json_reserv) {
-		const message_code = json_reserv.message_code;
-		var jsonString = "response not found";
-		if (message_code == 0) {
-			const all_reservations = await this.getAllRerservation();
-			jsonString = JSON.stringify(all_reservations);
-		}
-		if (message_code == 1) {
-			const delete_response = await this.deleteReservation(json_reserv.id);
-			jsonString = JSON.stringify(delete_response);
-		}
-		if (message_code == 2) {
-			const create_response = await this.createReservation(
-				json_reserv.userid,
-				json_reserv.timeid,
-				json_reserv.people,
-			);
-			jsonString = JSON.stringify(create_response);
-		}
-		if (message_code == 3) {
-			const reservation_ = await this.getReservationById(json_reserv.id);
-			jsonString = JSON.stringify(reservation_);
-		}
-		if (message_code == 4) {
-			const reserv_ = await this.getReservationByEmail(json_reserv.email);
-			jsonString = JSON.stringify(reserv_);
-		}
+  async process_message(json_reserv) {
+    const message_code = json_reserv.message_code;
+    var jsonString = "response not found";
+    if (message_code == 0) {
+      const all_reservations = await this.getAllRerservation();
+      jsonString = JSON.stringify(all_reservations);
+    }
+    if (message_code == 1) {
+      const delete_response = await this.deleteReservation(json_reserv.id);
+      jsonString = JSON.stringify(delete_response);
+    }
+    if (message_code == 2) {
+      const create_response = await this.createReservation(
+        json_reserv.userid,
+        json_reserv.timeid,
+        json_reserv.people,
+      );
+      jsonString = JSON.stringify(create_response);
+    }
+    if (message_code == 3) {
+      const reservation_ = await this.getReservationById(json_reserv.id);
+      jsonString = JSON.stringify(reservation_);
+    }
+    if (message_code == 4) {
+      const reserv_ = await this.getReservationByEmail(json_reserv.email);
+      jsonString = JSON.stringify(reserv_);
+    }
 
-		console.log("PubSub triggered - sending: ", jsonString);
-		this.pubSubHandler.send_message(jsonString);
-	}
+    console.log("PubSub triggered - sending: ", jsonString);
+    this.pubSubHandler.send_message(jsonString);
+  }
 
-	async getAllRerservation() {
-		const resp = await this.databaseController.getAllReservations();
-		return {
-			status: 202,
-			data: resp,
-		};
-	}
+  async getAllRerservation() {
+    const resp = await this.databaseController.getAllReservations();
+    return {
+      status: 202,
+      data: resp,
+    };
+  }
 
-	async getReservationById(id) {
-		const resp = await this.databaseController.getReservationByID(id);
-		if (resp === 1) {
-			return {
-				status: 401,
-				data: "Reservacion no encontrada",
-			};
-		} else {
-			return {
-				status: 202,
-				data: resp,
-			};
-		}
-	}
-	async deleteReservation(id) {
-		const resp = await this.databaseController.deleteReservation(id);
-		if (resp != 1) {
-			return {
-				status: 202,
-				data: resp,
-			};
-		} else {
-			return {
-				status: 401,
-				data: "Reservacion no encontrada",
-			};
-		}
-	}
-	async createReservation(userid, timeid, people) {
-		const resp = await this.databaseController.createNewRervation(
-			userid,
-			timeid,
-			people,
-		);
-		if (!resp) {
-			return {
-				status: 401,
-				data: "Error al crear la reservacion",
-			};
-		} else if (resp == 1) {
-			return {
-				status: 401,
-				data: "La fecha seleccionada no esta disponible",
-			};
-		} else if (resp == 2) {
-			return {
-				status: 401,
-				data: "Usuario no encontrado",
-			};
-		} else {
-			return {
-				status: 202,
-				data: resp,
-			};
-		}
-	}
-	async getReservationByEmail(email) {
-		const resp = await this.databaseController.getReservationByEmail(email);
-		if (resp == 1) {
-			return {
-				status: 404,
-				data: "Email no asociado a ningun usuario",
-			};
-		} else {
-			return {
-				status: 202,
-				data: resp,
-			};
-		}
-	}
+  async getReservationById(id) {
+    const resp = await this.databaseController.getReservationByID(id);
+    if (resp === 1) {
+      return {
+        status: 401,
+        data: "Reservacion no encontrada",
+      };
+    } else {
+      return {
+        status: 202,
+        data: resp,
+      };
+    }
+  }
+  async deleteReservation(id) {
+    const resp = await this.databaseController.deleteReservation(id);
+    if (resp != 1) {
+      return {
+        status: 202,
+        data: resp,
+      };
+    } else {
+      return {
+        status: 401,
+        data: "Reservacion no encontrada",
+      };
+    }
+  }
+  async createReservation(userid, timeid, people) {
+    const resp = await this.databaseController.createNewRervation(
+      userid,
+      timeid,
+      people,
+    );
+    if (!resp) {
+      return {
+        status: 401,
+        data: "Error al crear la reservacion",
+      };
+    } else if (resp == 1) {
+      return {
+        status: 401,
+        data: "La fecha seleccionada no esta disponible",
+      };
+    } else if (resp == 2) {
+      return {
+        status: 401,
+        data: "Usuario no encontrado",
+      };
+    } else {
+      return {
+        status: 202,
+        data: resp,
+      };
+    }
+  }
+  async getReservationByEmail(email) {
+    const resp = await this.databaseController.getReservationByEmail(email);
+    if (resp == 1) {
+      return {
+        status: 404,
+        data: "Email no asociado a ningun usuario",
+      };
+    } else {
+      return {
+        status: 202,
+        data: resp,
+      };
+    }
+  }
 }
 
 entry_function = async (cloud_message) => {
-	const pubsub_message = cloud_message.data.message;
-	// If local, uncomment
-	// pubsub_message.ack()
-	const reservationCore = new ReservationCore();
-	const msg_payload_str = Buffer.from(pubsub_message.data, "base64").toString();
-	await reservationCore.process_message(JSON.parse(msg_payload_str));
+  const pubsub_message = cloud_message.data.message;
+  // If local, uncomment
+  // pubsub_message.ack()
+  const reservationCore = new ReservationCore();
+  const msg_payload_str = Buffer.from(pubsub_message.data, "base64").toString();
+  await reservationCore.process_message(JSON.parse(msg_payload_str));
 };
 
 module.exports = { Reservation };
