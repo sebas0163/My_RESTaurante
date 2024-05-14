d8f83400b7c2b6e769cd56d199516d501ebbb32a
Kevin Zeledón Salazar <magneticok@gmail.com>
2024-05-14 10:24:05 -0600

    Revert "Reservacion (#17)" (#20)
    This reverts commit f26f44148ff1904a843fead00236282d150b2827.

--- BackendCore/RoutingComponent.js
+++ BackendCore/RoutingComponent.js
@@ -3,21 +3,14 @@ const router = express.Router();
 const { SentimentController } = require("./SentimentController");
 const { DishController } = require("./DishController");
 const { TimeController } = require("./TimeController");
-const { ReservationController } = require("./ReservationController");
 
 sentiment = new SentimentController();
 dish_controller = new DishController();
 time_reco = new TimeController();
-reservationController = new ReservationController();
 
 router.get("/Sentiment/:feedback?", sentiment.askForSentiment);
 router.get("/food/menu", dish_controller.getAllMenu);
 // router.post('/recomendation/time', time_reco.askSchedule);
 router.post("/food/recomendation", dish_controller.askForDish);
-router.get("/reservation/getAll", reservationController.getAllReservations);
-router.get("/reservation/getById:id?", reservationController.getReservationById);
-router.get("/reservation/getByEmail:email?", reservationController.getReservationByEmail);
-router.delete("/reservation/delete", reservationController.deleteReservation);
-router.post("/reservation/new", reservationController.createReservation);
 
 module.exports = router;
